40a607e51664b8af09248a27e69ac311e7d5dd1f
TomasQReis <tomas.qreis@gmail.com>
2026-08-04 14:18:13 +0200

    + Toggles for each of the main estimation steps

--- main.py
+++ main.py
@@ -15,134 +15,139 @@ from tudatpy.estimation.estimation_analysis import Estimator
 
 if __name__ == "__main__":
 
+    
+
     ### -----------------------------------------------------------------------
     ### Propagation of "reality".
     ### -----------------------------------------------------------------------
 
-    # Propagation start and end dates. 
-    # Given as [Year, Month, Day]
-    simStartDate    = [2026, 7, 31]
-    simEndDate      = [2026, 8, 1]
+    if propagateReality:= False:
+        # Propagation start and end dates. 
+        # Given as [Year, Month, Day]
+        simStartDate    = [2026, 7, 31]
+        simEndDate      = [2026, 8, 1]
 
-    # Propagation time step size. 
-    propTimeStep = 10.0
+        # Propagation time step size. 
+        propTimeStep = 10.0
 
-    # Load SPICE kernels and return J2000 formatted epochs. 
-    simStartEpoch, simEndEpoch = load_spice( 
-        startEpoch= simStartDate, endEpoch= simEndDate 
+        # Load SPICE kernels and return J2000 formatted epochs. 
+        simStartEpoch, simEndEpoch = load_spice( 
+            startEpoch= simStartDate, endEpoch= simEndDate 
+            )
+
+        # Choose list of spacecraft for simulation. 
+        spacecraftDicts = rociABList
+
+        # Set up true environment bodies. 
+        simulationBodies = environment_bodies_low_fidelity_true_model( 
+            spacecrafts= spacecraftDicts 
         )
 
-    # Choose list of spacecraft for simulation. 
-    spacecraftDicts = rociABList
+        
 
-    # Set up true environment bodies. 
-    simulationBodies = environment_bodies_low_fidelity_true_model( 
-        spacecrafts= spacecraftDicts 
-    )
+        # Set up propagator settings. 
+        propagatorSettings = environment_prop_settings_low_fidelity(
+            spacecrafts= spacecraftDicts,
+            bodies= simulationBodies,
+            simStartEpoch= simStartEpoch,
+            simEndEpoch= simEndEpoch,
+            timeStep= propTimeStep,
+            sHMoon= True,
+            keepEnvironment= True
+        )
 
-    
+        # Propagate orbit. 
+        stateHistory, dependentVarsHistory = environment_propagate_low_fidelity(
+            bodies= simulationBodies,
+            propagationSettings= propagatorSettings
+        )
 
-    # Set up propagator settings. 
-    propagatorSettings = environment_prop_settings_low_fidelity(
-        spacecrafts= spacecraftDicts,
-        bodies= simulationBodies,
-        simStartEpoch= simStartEpoch,
-        simEndEpoch= simEndEpoch,
-        timeStep= propTimeStep,
-        sHMoon= True,
-        keepEnvironment= True
-    )
-
-    # Propagate orbit. 
-    stateHistory, dependentVarsHistory = environment_propagate_low_fidelity(
-        bodies= simulationBodies,
-        propagationSettings= propagatorSettings
-    )
-
-    # Convert reality propagation output into NDArrays. 
-    dependentVarsHistoryArray = result2array(dependentVarsHistory)
-    stateHistoryArr = result2array(stateHistory)
+        # Convert reality propagation output into NDArrays. 
+        dependentVarsHistoryArray = result2array(dependentVarsHistory)
+        stateHistoryArr = result2array(stateHistory)
 
     ### -----------------------------------------------------------------------
     ### Simulating observations.
     ### -----------------------------------------------------------------------
 
-    # Create set of observation times. 
-    observationsStep = 10.0
-    observationTimes = np.arange( simStartEpoch+observationsStep, 
-                                step= observationsStep,
-                                stop= simEndEpoch)
+    if simulateObservations:= False:
+        # Create set of observation times. 
+        observationsStep = 10.0
+        observationTimes = np.arange( simStartEpoch+observationsStep, 
+                                    step= observationsStep,
+                                    stop= simEndEpoch)
 
-    # Create observation links.
-    observationLinkDefinition = link_creation_owr(
-        transmitterDict= spacecraftDicts[0],
-        receiverDict= spacecraftDicts[1]
-    )
+        # Create observation links.
+        observationLinkDefinition = link_creation_owr(
+            transmitterDict= spacecraftDicts[0],
+            receiverDict= spacecraftDicts[1]
+        )
 
-    # Create observation model simulator.
-    observationModelSimulator, observationModelSettings = observation_model_simulator_owr(
-        owrLinkDefinition= observationLinkDefinition,
-        bodies= simulationBodies
-    )
+        # Create observation model simulator.
+        observationModelSimulator, observationModelSettings = observation_model_simulator_owr(
+            owrLinkDefinition= observationLinkDefinition,
+            bodies= simulationBodies
+        )
 
-    # Simulate observations. 
-    simulatedObservations, simulatedObservationsDependentVars = simulate_observations_owr(
-        observationTimes= observationTimes.tolist(),
-        owrLinkDefinition= observationLinkDefinition,
-        owrObservationSimulator= observationModelSimulator,
-        bodies= simulationBodies
-    )
+        # Simulate observations. 
+        simulatedObservations, simulatedObservationsDependentVars = simulate_observations_owr(
+            observationTimes= observationTimes.tolist(),
+            owrLinkDefinition= observationLinkDefinition,
+            owrObservationSimulator= observationModelSimulator,
+            bodies= simulationBodies
+        )
 
-    # Extract simulated observation data. 
-    simulatedObservationValues, simulatedObservationTimes = extract_data_observations_owr(
-        observationCollection= simulatedObservations
-    )
+        # Extract simulated observation data. 
+        simulatedObservationValues, simulatedObservationTimes = extract_data_observations_owr(
+            observationCollection= simulatedObservations
+        )
 
     ### -----------------------------------------------------------------------
     ### Estimating parameters.
     ### -----------------------------------------------------------------------
 
-    # Set up estimation environment bodies. 
-    estimationBodies = environment_bodies_low_fidelity_estimation_model(
-        spacecrafts= spacecraftDicts
-    )
-
-    # Set up estimation propagation settings. 
-    estimationPropSettings = environment_prop_settings_low_fidelity(
-        spacecrafts= spacecraftDicts,
-        bodies= estimationBodies,
-        simStartEpoch= simStartEpoch,
-        simEndEpoch= simEndEpoch,
-        timeStep= propTimeStep,
-        sHMoon= False
-    )
-
-    # Set up desired estimation parameters.
-    parameterSettings = dynamics.parameters_setup.initial_states( 
-        estimationPropSettings, estimationBodies 
+    if estimateParameters:= False:
+        # Set up estimation environment bodies. 
+        estimationBodies = environment_bodies_low_fidelity_estimation_model(
+            spacecrafts= spacecraftDicts
         )
-    parameterSet = dynamics.parameters_setup.create_parameter_set(
-        parameter_settings= parameterSettings,
-        bodies= estimationBodies
+
+        # Set up estimation propagation settings. 
+        estimationPropSettings = environment_prop_settings_low_fidelity(
+            spacecrafts= spacecraftDicts,
+            bodies= estimationBodies,
+            simStartEpoch= simStartEpoch,
+            simEndEpoch= simEndEpoch,
+            timeStep= propTimeStep,
+            sHMoon= False
         )
-    
-    # Creating estimator. 
-    estimator = Estimator(
-        bodies= estimationBodies,
-        estimated_parameters= parameterSet,
-        observation_settings= observationModelSettings,
-        propagator_settings= estimationPropSettings
-    )
 
-    # Estimation settings. 
-    estimationSettings = estimation.estimation_analysis.EstimationInput(
-        observations_and_times= simulatedObservations
-    )
+        # Set up desired estimation parameters.
+        parameterSettings = dynamics.parameters_setup.initial_states( 
+            estimationPropSettings, estimationBodies 
+            )
+        parameterSet = dynamics.parameters_setup.create_parameter_set(
+            parameter_settings= parameterSettings,
+            bodies= estimationBodies
+            )
+        
+        # Creating estimator. 
+        estimator = Estimator(
+            bodies= estimationBodies,
+            estimated_parameters= parameterSet,
+            observation_settings= observationModelSettings,
+            propagator_settings= estimationPropSettings
+        )
 
-    # Perform parameter estimation.
-    estimationOutput = estimator.perform_estimation(
-        estimation_input= estimationSettings
-    )
+        # Estimation settings. 
+        estimationSettings = estimation.estimation_analysis.EstimationInput(
+            observations_and_times= simulatedObservations
+        )
+
+        # Perform parameter estimation.
+        estimationOutput = estimator.perform_estimation(
+            estimation_input= estimationSettings
+        )
 
     ### -----------------------------------------------------------------------
 
